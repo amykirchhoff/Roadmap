fefbe52198ae15332dcf88dfdb4d2acdc1011452
Amy Kirchhoff <mylifewithfour@gmail.com>
2026-03-06 19:52:58 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Set this to your GitHub repo name, e.g. '/roadmap-explorer/'
-// If you rename the repo, update this value to match.
 const BASE_PATH = '/Roadmap/'
 
 export default defineConfig({
